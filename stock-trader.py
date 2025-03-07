69bc7fa09f175b8b1c3c2368c004232311e533a7
Stoverbrock1 <ztgstover@berkeley.edu>
2025-03-06 16:54:52 -0800

    setting up necessary classes and wrapper

--- stock-trader.py
+++ stock-trader.py
@@ -3,11 +3,24 @@ import random
 import time
 
 
+#class order:
+
+
+#class queue:
+
+
+#class orderBook:
 
 
 
 
+
+def randomOrders(order_book):
+    tickers = ["STK{}".format(i) for i in range(1024)]
+    print(len(tickers))
 
 
 if (__name__ == "__main__"):
     print(0)
+    order_book = 0 
+    randomOrders(order_book)
